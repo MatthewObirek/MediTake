c00931d6d5e85a3f8a9b930b41820252d9d3d8a0
MatthewObirek <matthewobirek@gmail.com>
2022-11-27 15:29:40 -0800

    edited README and added Title to xml page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cosc341Group14
 Cosc 341 Group 14 Project Step 4
+
+
+### Connection
+The Connection section mainly centers around the ProfileActivity.
--- app/src/main/java/com/example/cosc341group14/profileActivity.java
+++ app/src/main/java/com/example/cosc341group14/profileActivity.java
@@ -13,6 +13,8 @@ public class profileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+
+
         getSupportActionBar().setTitle("Patients");
     }
 
